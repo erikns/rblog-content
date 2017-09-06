ef1e33a80f2922e5504443ce44e0999fdee49a81
Erik Sørensen <eriksorensen88@gmail.com>
2017-09-06 11:36:20 +0200

    Use todays date for post creation when none given

--- tools/rblog-create.js
+++ tools/rblog-create.js
@@ -1,10 +1,16 @@
 const util = require('./util');
+const dateformat = require('dateformat');
 const commander = require('commander');
 
 commander
-    .arguments('<slug>')
-    .action((slug) => {
+    .arguments('<slug> [date]')
+    .action((slug, date) => {
         createSlug = slug;
+        createDate = date;
+        if (!date) {
+            console.log('Date is not given. Using today');
+            createDate = dateformat(new Date(), 'dd-mm-yyyy');
+        }
     });
 
 commander.parse(process.argv);
@@ -14,5 +20,5 @@ if (typeof createSlug === 'undefined') {
     process.exit(1);
 }
 
-const postFile = util.addPost(createSlug, '12-12-2017');
+const postFile = util.addPost(createSlug, createDate);
 console.log("Created post file: '" + postFile + "'");
